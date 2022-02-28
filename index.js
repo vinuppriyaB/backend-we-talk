4f1ba0e77cc342a02f8d44d79ad60d6547ee59ca
vinuppriya balachandran <vinuppriya.b@gmail.com>
2022-02-28 20:06:10 +0530

    procfile added

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://zealous-morse-54b38e.netlify.app",
   },
 });
 
